3fcbc3e3d61c2b8843b8933b4bc73065984dccf5
Aaron Roth <aroth3.quentin@gmail.com>
2012-05-24 17:42:15 -0400

    housekeeping

---
+++ driver.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012 Aaron Roth
+# See the file license.txt for copying permission.
+#
+
+import getopt
+import serial
+import sys
+import time
+import urllib2
+
+pour_time = 10
+
+def usage():
+  print 'usage: ' + sys.argv[0] + ' -p <port>'
+
+def main():
+  # Read in command line arguments.
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hp:', ['help', 'port='])
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+  
+  port = None
+  
+  # Specify a port.
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+      usage()
+      sys.exit(0)
+    if opt in ('-p', '--port'):
+      port = arg
+  
+  # Make sure a serial port was specified.
+  if not port:
+    usage()
+    sys.exit(2)
+  
+  # Open the specified serial port.
+  try:
+    ser = serial.Serial(port=port, baudrate=9600, timeout=1)
+  except serial.SerialException:
+    print 'error: unable to open the specified serial port'
+    sys.exit(1)
+  
+  # Request drink statuses from the server and send to the Arduino.
+  while True:
+    # Get current drink values.
+    request_object = urllib2.urlopen('http://findaaron.nfshost.com/Robolette/php/get_counts.php')
+    pour_states = request_object.read()
+    team_one_state = int(pour_states[0])
+    team_two_state = int(pour_states[1])
+    
+    # Decide which valves to open if any.
+    if team_one_state > 0 and team_two_state == 0:
+      ser.write('10')
+      time.sleep(pour_time)
+    elif team_one_state == 0 and team_two_state > 0:
+      ser.write('01')
+      time.sleep(pour_time)
+    elif team_one_state > 0 and team_two_state > 0:
+      ser.write('11')
+      time.sleep(pour_time)
+
+if __name__ == '__main__':
+  main()
---
+++ server/get_counts.php
@@ -0,0 +1,40 @@
+<?php
+  // Copyright (c) 2012 Aaron Roth
+  // See the file license.txt for copying permission.
+  //
+  
+  try
+  {
+    $team_one_count;
+    $team_two_count;
+    
+    // Open a connection to the database.
+    $db_handle = new PDO('sqlite:../drinks.db');
+    
+    $result = $db_handle->query("SELECT * FROM drinkcounts
+                                 WHERE name = 'counts'");
+    
+    foreach ($result as $row)
+    {
+      $team_one_count = intval($row[1]);
+      $team_two_count = intval($row[2]);
+    }
+    
+    // Respond with the current drink counts.
+    echo $team_one_count . $team_two_count;
+    
+    // Reduce each count in the database by one.
+    if ($team_one_count > 0)
+      $db_handle->exec("UPDATE drinkcounts SET team_one = team_one - 1");
+    
+    if ($team_two_count > 0)
+      $db_handle->exec("UPDATE drinkcounts SET team_two = team_two - 1");
+    
+    // Close the database connection.
+    $db_handle = null;
+  }
+  catch (PDOException $exception)
+  {
+    echo $exception->getMessage();
+  }
+?>
