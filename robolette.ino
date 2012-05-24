7314221126e6c1adb3e013539272d452c6256bd5
Aaron Roth <aroth3.quentin@gmail.com>
2012-05-24 17:39:31 -0400

    housekeeping

--- robolette.ino
+++ robolette.ino
@@ -2,22 +2,22 @@
 // See the file license.txt for copying permission.
 //
 
+const int pour_time = 1000;
 const int baud_rate = 9600;
 const int buffer_size = 2;
-const int right_valve = 8;
 const int left_valve = 11;
-const int pour_time = 1000;
+const int right_valve = 8;
 
 // Buffer and variables to store incoming valve pour states.
 int buffer[buffer_size];
-int right_v_state;
 int left_v_state;
+int right_v_state;
 
 void setup()
 {
   Serial.begin(baud_rate);
-  pinMode(8, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop()
@@ -32,28 +32,28 @@ void loop()
       delay(5);
     }
     
-    right_v_state = buffer[0];
-    left_v_state = buffer[1];
+    left_v_state = buffer[0];
+    right_v_state = buffer[1];
     
-    if (right_v_state == 1 && left_v_state == 0)
-    {
-      digitalWrite(right_valve, HIGH);
-      delay(pour_time);
-      digitalWrite(right_valve, LOW);
-    }
-    else if (right_v_state == 0 && left_v_state == 1)
+    if (left_v_state == 1 && right_v_state == 0)
     {
       digitalWrite(left_valve, HIGH);
       delay(pour_time);
       digitalWrite(left_valve, LOW);
     }
-    else if (right_v_state == 1 && left_v_state == 1)
+    else if (left_v_state == 0 && right_v_state == 1)
     {
       digitalWrite(right_valve, HIGH);
-      digitalWrite(left_valve, HIGH);
       delay(pour_time);
       digitalWrite(right_valve, LOW);
+    }
+    else if (left_v_state == 1 && right_v_state == 1)
+    {
+      digitalWrite(left_valve, HIGH);
+      digitalWrite(right_valve, HIGH);
+      delay(pour_time);
       digitalWrite(left_valve, LOW);
+      digitalWrite(right_valve, LOW);
     }
   }
 }
