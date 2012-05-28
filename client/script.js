4cd9c971a922f2d64d8a9dc2738ec6374f8ec3a4
Aaron Roth <aroth3.quentin@gmail.com>
2012-05-28 13:23:00 -0400

    successful roulette wheel drawing

--- client/script.js
+++ client/script.js
@@ -1,2 +1,110 @@
+// Copyright (c) 2012 Aaron Roth
+// See the file license.txt for copying permission.
+//
+
+// ------------------ Globals ------------------ //
+
+// Canvas.
+var canvasX = 250;
+var canvasY = 250;
+
+// Wheel positioning.
+var arc = Math.PI / 18.5;
+var context;
+var insideRadius = 175;
+var outsideRadius = 200;
+var startAngle = -89.6;
+var textRadius = 184;
+
+// Wheel style and content.
+var black = '#000000';
+var fontStyle = 'bold 12px sans-serif';
+var green = '#006600';
+var lineWidth = 2;
+var red = '#ff0000';
+var white = '#ffffff';
+var numbers = ['0', '32', '15', '19', '4', '21', '2', '25', '17', '34', '6',
+               '27', '13', '36', '11', '30', '8', '23', '10', '5', '24', '16',
+               '33', '1', '20', '14', '31', '9', '22', '18', '29', '7', '28',
+               '12', '35', '3', '26'];
+
+// Wheel spinning.
+var spinArcStart = 10;
+var spinTime = 0;
+var spinTimeout = null;
+var spinTimeTotal = 0;
+
+// --------------------------------------------- //
+
 $(document).ready(function() {
+  $('#start-button').click(start_game);
 });
+
+function draw_wheel() {
+  var canvas = document.getElementById('wheel');
+  
+  // Make sure the browser supports canvas.
+  if (canvas.getContext) {
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, 500, 500);
+    
+    // Set wheel styles.
+    context.strokeStyle = black;
+    context.lineWidth = lineWidth;
+    context.font = fontStyle;
+    
+    // Draw entire wheel.
+    for (var i = 0; i < 37; i++) {
+      var angle = startAngle + i * arc;
+      
+      // Set colors.
+      if (i == 0) {
+        context.fillStyle = green;
+      } else if (i % 2 != 0) {
+        context.fillStyle = red;
+      } else {
+        context.fillStyle = black;
+      }
+      
+      // Draw wheel.
+      context.beginPath();
+      context.arc(canvasX, canvasY, outsideRadius, angle, angle + arc, false);
+      context.arc(canvasX, canvasY, insideRadius, angle + arc, angle, true);
+      context.stroke();
+      context.fill();
+      context.save();
+      
+      // Draw numbers on the wheel.
+      var text = numbers[i];
+      var transX = canvasX + Math.cos(angle + arc / 2) * textRadius;
+      var transY = canvasY + Math.sin(angle + arc / 2) * textRadius;
+      var rotateVal = angle + arc / 2 + Math.PI / 2;
+      
+      context.translate(transX, transY);
+      context.rotate(rotateVal);
+      context.fillStyle = white;
+      context.fillText(text, -context.measureText(text).width / 2, 0);
+      context.restore();
+    }
+  } else {
+    alert('Your browser does not support this game! ' +
+          'Try Safari, Chrome, Firefox, or Opera.');
+  }
+}
+
+// Sets up the game board.
+function start_game() {
+  var startBank = $('#starting-bank').val();
+  var minBet = $('#min-bet').val();
+  var maxBet = $('#max-bet').val();
+  
+  $('#form').fadeOut(100, 'linear', function() {
+    $('#form').remove();
+  });
+  $('#title').fadeOut(100, 'linear', function() {
+    $('#title').remove();
+  });
+  
+  $('#game-board').fadeIn(100, 'linear');
+  draw_wheel();
+}
