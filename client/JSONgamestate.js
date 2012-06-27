fd84fd8e2cebef5e0be28c9185dd9c7afa9d9260
Aaron Roth <aroth3.quentin@gmail.com>
2012-06-27 00:42:50 -0400

    completed game stateobject

---
+++ client/JSONgamestate.js
@@ -0,0 +1,665 @@
+// Game state object.
+var gameState = {
+  "bets": {
+    "bet-straightup-0": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-1": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-2": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-4": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-5": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-7": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-8": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-10": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-11": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-13": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-14": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-16": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-17": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-19": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-20": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-22": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-23": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-25": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-26": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-28": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-29": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-31": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-32": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-34": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-35": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-straightup-36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-0|3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-3|6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-6|9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-9|12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-12|15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-15|18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-18|21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-21|24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-24|27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-27|30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-30|33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-33|36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-2|3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-5|6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-8|9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-11|12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-14|15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-17|18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-20|21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-23|24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-26|27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-29|30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-32|33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-35|36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-0|2": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-2|5": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-5|8": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-8|11": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-11|14": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-14|17": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-17|20": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-spit-20|23": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-23|26": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-26|29": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-29|32": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-32|35": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-1|2": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-4|5": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-7|8": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-10|11": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-13|14": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-16|17": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-19|20": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-19|20": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-22|23": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-25|26": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-28|29": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-31|32": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-34|35": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-0|1": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-1|4": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-4|7": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-7|10": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-10|13": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-13|16": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-16|19": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-19|22": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-22|25": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-25|28": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-28|31": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-split-31|34": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-2|3|5|6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-5|6|8|9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-8|9|11|12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-11|12|14|15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-14|15|17|18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-17|18|20|21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-20|21|23|24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-23|24|26|27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-26|27|29|30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-29|30|32|33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-32|33|35|36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-1|2|4|5": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-4|5|7|8": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-7|8|10|11": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-10|11|13|14": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-13|14|16|17": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-16|17|19|20": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-19|20|22|23": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-22|23|25|26": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-25|26|28|29": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-28|29|31|32": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-31|32|34|35": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-corner-0|1|2|3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-1|2|3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-4|5|6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-7|8|9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-10|11|12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-13|14|15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-16|17|18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-19|20|21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-22|23|24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-25|26|27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-28|29|30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-31|32|33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-street-34|35|36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-1|2|3|4|5|6": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-4|5|6|7|8|9": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-7|8|910|11|12": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-10|11|12|13|14|15": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-13|14|15|16|17|18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-16|17|18|19|20|21": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-19|20|21|22|23|24": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-22|23|24|25|26|27": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-25|26|27|28|29|30": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-28|29|30|31|32|33": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-sixline-31|32|33|34|35|36": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-basket-0|2|3": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-basket-0|1|2": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-column-3rd": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-column-2nd": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-column-1st": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-dozen-1st": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-dozen-2nd": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-dozen-3rd": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-half-1to18": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-other-even": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-other-red": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-other-black": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-other-odd": {
+      "team1": 0,
+      "team2": 0
+    },
+    "bet-half-19to36": {
+      "team1": 0,
+      "team2": 0
+    }
+  },
+  "chips": {
+    "team1": 0,
+    "team2": 0
+  },
+  "drinks": {
+    "team1": 0,
+    "team2": 0
+  },
+  "max": 0,
+  "min": 0,
+  "scores": {
+    "team1": 0,
+    "team2": 0
+  },
+  "state": {
+    "changes": 1,
+    "player": "none",
+    "spins": 1,
+    "undo": 0
+  },
+  "totals": {
+    "team1": 0,
+    "team2": 0
+  },
+  "wheel": {
+    "color": "",
+    "number": 0
+  }
+};
